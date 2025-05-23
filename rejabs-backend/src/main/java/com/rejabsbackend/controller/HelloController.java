24ebb4e61159333088de879f6ec83b0fa3de4d1d
Mimoona <memoona.raheel8@gmail.com>
2025-05-23 14:06:51 +0200

    add sonar setup for backend

---
+++ rejabs-backend/src/main/java/com/rejabsbackend/controller/HelloController.java
@@ -0,0 +1,9 @@
+package com.rejabsbackend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/hello")
+public class HelloController {
+}
