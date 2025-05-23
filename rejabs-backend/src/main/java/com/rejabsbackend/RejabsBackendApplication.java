f83c17ce0a73a801a87f848ff2e907672b548f7a
Mimoona <memoona.raheel8@gmail.com>
2025-05-23 12:10:30 +0200

    add backend and frontend modules

---
+++ rejabs-backend/src/main/java/com/rejabsbackend/RejabsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rejabsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RejabsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RejabsBackendApplication.class, args);
+    }
+
+}
---
+++ rejabs-backend/src/test/java/com/rejabsbackend/RejabsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rejabsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RejabsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
