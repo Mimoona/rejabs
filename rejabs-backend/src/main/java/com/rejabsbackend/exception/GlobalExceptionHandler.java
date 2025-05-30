cc06cfae9fd3472d164908102e39b6bd49696cb7
Mimoona <memoona.raheel8@gmail.com>
2025-05-30 15:34:53 +0200

    remove unused imports

--- rejabs-backend/src/main/java/com/rejabsbackend/exception/GlobalExceptionHandler.java
+++ rejabs-backend/src/main/java/com/rejabsbackend/exception/GlobalExceptionHandler.java
@@ -1,14 +1,7 @@
 package com.rejabsbackend.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.time.Instant;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
