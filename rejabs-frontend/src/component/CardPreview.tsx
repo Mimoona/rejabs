8d0537eaa0ae5446d68c4620f976b8519deb9949
Mimoona <memoona.raheel8@gmail.com>
2025-06-19 18:47:18 +0200

    resolve non-interactive issue frontend from sonar

--- rejabs-frontend/src/component/CardPreview.tsx
+++ rejabs-frontend/src/component/CardPreview.tsx
@@ -7,16 +7,23 @@ interface Props {
     onClick: () => void;
 }
 
-const CardPreview = ({ card, onClick }: Props) => {
+const CardPreview = ({card, onClick}: Props) => {
 
     return (
         <div
+            role="button"
+            tabIndex={0}
             onClick={onClick}
+            onKeyDown={(e) => {
+                if (e.key === 'Enter' || e.key === ' ') {
+                    onClick(e);
+                }
+            }}
             className="bg-gray-200 hover:bg-gray-300 rounded p-2 cursor-pointer shadow-sm transition"
         >
             <div className="flex items-center justify-between">
-            <h3 className="text-sm font-medium text-gray-800 truncate">{card.cardTitle}</h3>
-            <PencilIcon className="h-4 w-4" onClick={onClick}></PencilIcon>
+                <h3 className="text-sm font-medium text-gray-800 truncate">{card.cardTitle}</h3>
+                <PencilIcon className="h-4 w-4" onClick={onClick}></PencilIcon>
             </div>
             <div>
                 <p className="text-sm text-gray-600 truncate my-4">{card.description}</p>
@@ -27,13 +34,13 @@ const CardPreview = ({ card, onClick }: Props) => {
                     <span
                         key={label}
                         className="w-3 h-3 rounded-full"
-                        style={{ backgroundColor: getColorFromLabel(label) }}
+                        style={{backgroundColor: getColorFromLabel(label)}}
                     />
                 ))}
 
                 {card.dueDate && (
                     <span title={card.dueDate}>
-            <CalendarIcon className="w-4 h-4 text-gray-500" />
+            <CalendarIcon className="w-4 h-4 text-gray-500"/>
 
           </span>
                 )}
