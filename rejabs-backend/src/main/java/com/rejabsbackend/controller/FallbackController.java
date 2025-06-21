edbef075ff205ec4904d870e0497602bb68bf781
Mimoona <memoona.raheel8@gmail.com>
2025-06-21 17:04:20 +0200

    Âadd Fallback controller to handle non api routes

---
+++ rejabs-backend/src/main/java/com/rejabsbackend/controller/FallbackController.java
@@ -0,0 +1,16 @@
+package com.rejabsbackend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class FallbackController {
+
+    @GetMapping(value = {
+            "/", "/signup", "/home", "/profile", "/boards/**"
+    })
+    public String forward() {
+        return "forward:/index.html";
+    }
+}
